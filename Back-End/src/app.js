6a7be0a9e72443ce691a470917b9bd84a6042049
hariskhn <harislif3@gmail.com>
2025-02-23 18:55:34 +0500

    Add user routes for registration, login, logout, and token refresh

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -12,4 +12,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+//ROUTES IMPORT
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter);
+
 export { app }; 
